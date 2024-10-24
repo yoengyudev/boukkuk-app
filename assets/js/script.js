c7a1ea9047094b74be381b0576e60ec1f85c4ad2
Yoeng Yu <alienwarcc01@gmail.com>
2024-10-24 15:22:46 +0700

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,68 @@
 let g9_host = "https://mps10.chandalen.dev";
+let isAuthenticated = false;
+let token = localStorage.getItem("token");
 
+if (token) {
+    isAuthenticated = true;
+    console.log(token);
+}
+
+
+console.log(isAuthenticated);
+
+
+if(!isAuthenticated) {
+    let logout_list = document.getElementById("logout-list");
+    if(logout_list) {
+        logout_list.style.display = "none";
+    }
+}
 
 
 // ============================ login ============================
-let login = document.getElementById("login");
+const login = document.getElementById("login");
 
-login.addEventListener("submit", (even) => {
-    even.preventDefault();
-    let formdata = new FormData();
-
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    formdata.append('email_or_phone', email);
-    formdata.append('password', password);
+if (login) {
+    login.addEventListener("submit", (even) => {
+        even.preventDefault();
+        let formdata = new FormData();
     
-    fetch(`${g9_host}/api/login`,{
-        method: "POST",
-        body: formdata,
-        headers: {
-            'Accept': 'application/json'
-        }
-    })
-        .then(res=>res.json())
-        .then(json=>{
-            let token = json.data.token;
-            localStorage.setItem('token', token);
-            if(token) {
-                location.href = '../../../index.html';
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
+        formdata.append('email_or_phone', email);
+        formdata.append('password', password);
+    
+        fetch(`${g9_host}/api/login`, {
+            method: "POST",
+            body: formdata,
+            headers: {
+                'Accept': 'application/json'
             }
-        });
-});
+        })
+            .then(res => res.json())
+            .then(json => {
+                let token = json.data.token;
+                localStorage.setItem('token', token);
+                if (token) {
+                    location.href = '../../../index.html';
+                }
+            });
+    });
+}
 
 //============================================== end of login ========================
 
 // ============================ logout ============================
 
+let logout = document.getElementById("logout-list");
+console.log(logout);
 
+
+if (logout) {
+    logout.addEventListener('click', () => {
+        localStorage.removeItem('token');
+        window.location.reload();
+    });
+}
 
 // ============================ end of logout ============================
