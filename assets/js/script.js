1fcffe9946eec1e74f374cbe6819480573f75bf3
Yoeng Yu <alienwarcc01@gmail.com>
2024-10-24 15:31:07 +0700

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,45 +12,10 @@ console.log(isAuthenticated);
 
 
 if(!isAuthenticated) {
-    let logout_list = document.getElementById("logout-list");
-    if(logout_list) {
-        logout_list.style.display = "none";
-    }
+    document.getElementById("logout-list").style.display = "none";
 }
 
 
-// ============================ login ============================
-const login = document.getElementById("login");
-
-if (login) {
-    login.addEventListener("submit", (even) => {
-        even.preventDefault();
-        let formdata = new FormData();
-    
-        let email = document.getElementById("email").value;
-        let password = document.getElementById("password").value;
-        formdata.append('email_or_phone', email);
-        formdata.append('password', password);
-    
-        fetch(`${g9_host}/api/login`, {
-            method: "POST",
-            body: formdata,
-            headers: {
-                'Accept': 'application/json'
-            }
-        })
-            .then(res => res.json())
-            .then(json => {
-                let token = json.data.token;
-                localStorage.setItem('token', token);
-                if (token) {
-                    location.href = '../../../index.html';
-                }
-            });
-    });
-}
-
-//============================================== end of login ========================
 
 // ============================ logout ============================
 
