4da1e3b7387c36045d0c7aacdf575abfb154186b
Yoeng Yu <alienwarcc01@gmail.com>
2024-10-30 15:56:10 +0700

    update

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -21,6 +21,65 @@ function getData() {
 }
 getData();
 
+
+//===============================>> Update User Information Function <<================================//
+
+document.getElementById("updateForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let updateName = document.getElementById('fullname').value;
+    let updateEmail = document.getElementById('email').value;
+    let updatePhone = document.getElementById('phone').value;
+    let updateAddress = document.getElementById('address').value;
+    let updateButton = document.getElementById('updateButton');
+    let spinner = document.getElementById('spinner');
+    let buttonText = document.getElementById('buttonText');
+
+    updateButton.disabled = true;
+    spinner.style.display = 'inline-block';
+    buttonText.textContent = 'កំពុងរក្សាទុក...';
+
+    let token = localStorage.getItem("token");
+
+    if (!token) {
+        updateButton.disabled = false;
+        spinner.style.display = 'none';
+        buttonText.textContent = 'រក្សាទុកការផ្លាស់ប្តូរ';
+        return;
+    }
+
+    let updatedData = {
+        name: updateName,
+        email: updateEmail,
+        phone: updatePhone,
+        address: updateAddress,
+    };
+    console.log(updatedData.address);
+    fetch(`${g9_host}/api/profile/info`, {    
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + token,
+        },
+        body: JSON.stringify(updatedData)
+    })
+        .then(res => res.json())
+        .then(response => {
+            console.log(response.data);
+            console.log(response.data.name);
+            console.log(response.data.email);
+            console.log(response.data.phone);
+            console.log(response.data.address);
+        })
+        .finally(() => {
+            updateButton.disabled = false;
+            spinner.style.display = 'none';
+            buttonText.textContent = 'រក្សាទុកការផ្លាស់ប្តូរ';
+        });
+});
+
+// ======================================ending =================================//
+
 // =====================change password========================
 document
   .getElementById("changePasswordButton")
